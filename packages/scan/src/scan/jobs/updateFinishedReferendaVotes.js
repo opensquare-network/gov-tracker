bd4c7c9f0d7d853eb714aa94345e7e2f9f551bea
notset <notset@email.com>
2025-05-12 17:10:49 +0800

    Update finished referenda votes, #2

--- packages/scan/src/scan/jobs/updateFinishedReferendaVotes.js
+++ packages/scan/src/scan/jobs/updateFinishedReferendaVotes.js
@@ -1,27 +1,19 @@
 const {
   chain: { getApi },
 } = require("@osn/scan-common");
-const chunk = require("lodash.chunk");
 const { getFinishedReferenda } = require("../../store");
 const {
-  saveVotesForOneFinishedReferendum,
+  updateVotesForOneFinishedReferendum,
 } = require("../../scripts/referenda/finished");
 
 async function updateFinishedReferendaVotes(blockHeight) {
   const api = await getApi();
   const finishedReferenda = getFinishedReferenda(blockHeight);
-  const finishedReferendaChunk = chunk(finishedReferenda, 10);
-  for (const chunk of finishedReferendaChunk) {
-    const promises = [];
-    for (const referendumIndex of chunk) {
-      promises.push(
-        saveVotesForOneFinishedReferendum(api, {
-          referendumIndex,
-          voteFinishedHeight: blockHeight,
-        })
-      );
-    }
-    await Promise.all(promises);
+  for (const referendumIndex of finishedReferenda) {
+    await updateVotesForOneFinishedReferendum(api, {
+      referendumIndex,
+      voteFinishedHeight: blockHeight,
+    });
   }
 }
 
